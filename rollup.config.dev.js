a12ab9a1f4de4d58c6b2ee207ab7efa35ac4cc2f
Viet Ngoc <ngocjohn@me.com>
2026-05-23 01:45:28 +0200

    chore: add development configuration for Rollup with live server support

---
+++ rollup.config.dev.js
@@ -0,0 +1,33 @@
+import typescript from '@rollup/plugin-typescript';
+import json from '@rollup/plugin-json';
+import terser from '@rollup/plugin-terser';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import serve from 'rollup-plugin-serve';
+
+export default {
+  plugins: [
+    nodeResolve(),
+    json(),
+    typescript({}),
+    terser({
+      output: {
+        comments: false,
+      },
+    }),
+    serve({
+      contentBase: ['dist'],
+      port: 8235,
+      allowCrossOrigin: true,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+    }),
+  ],
+  input: 'src/custom-more-info.ts',
+  output: {
+    file: 'dist/custom-more-info.js',
+    format: 'iife',
+    sourcemap: true,
+    inlineDynamicImports: true,
+  },
+};
