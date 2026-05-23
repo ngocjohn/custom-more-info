b87a5ba638dcfafa022b5fadddf5c89a1bd76fc0
Viet Ngoc <ngocjohn@me.com>
2026-05-23 02:38:55 +0200

    chore: update dependencies and update build config

--- rollup.config.js
+++ rollup.config.js
@@ -2,21 +2,25 @@ import typescript from '@rollup/plugin-typescript';
 import json from '@rollup/plugin-json';
 import terser from '@rollup/plugin-terser';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-	plugins: [
-		nodeResolve(),
-		json(),
-		typescript(),
-		terser({
-			output: {
-				comments: false
-			}
-		})
-	],
-	input: 'src/custom-more-info.ts',
-	output: {
-		file: './custom-more-info.js',
-		format: 'iife'
-	}
+  plugins: [
+    nodeResolve({
+      preferBuiltins: false,
+    }),
+    commonjs(),
+    json(),
+    typescript({}),
+    terser({
+      output: {
+        comments: false,
+      },
+    }),
+  ],
+  input: 'src/custom-more-info.ts',
+  output: {
+    file: './custom-more-info.js',
+    format: 'iife',
+  },
 };
